6957131daec347b6cfdf50280d71d1bfe6aa16a0
KUTC-KaedeYuto <g02032@ktc.ac.jp>
2024-10-13 05:10:23 +0900

    回答提出用APIが引数を受けとれるように変更

--- src/preload.js
+++ src/preload.js
@@ -2,9 +2,9 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 
 contextBridge.exposeInMainWorld("proconApi", {
-    getProblem: () => ipcRenderer.send("getProblem"),
-    onProblemRespond: (callback) => ipcRenderer.on("responseProblem", (_event, value) => callback(value)),
+  getProblem: () => ipcRenderer.send("getProblem"),
+  onProblemRespond: (callback) => ipcRenderer.on("responseProblem", (_event, value) => callback(value)),
 
-    submitProblem: () => ipcRenderer.send("submitProblem"),
-    onAnswerRespond: (callback) => ipcRenderer.on("responseAnswer", (_event, value) => callback(value))
+  submitAnswer: (data) => ipcRenderer.send("submitAnswer", data),
+  onAnswerRespond: (callback) => ipcRenderer.on("responseAnswer", (_event, value) => callback(value))
 });
